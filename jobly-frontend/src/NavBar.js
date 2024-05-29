e418588c24836cef9338b9e3b6878d1fd78aa37b
Kruti Rathod <swapsmagic83@gmail.com>
2024-05-28 22:11:40 -0700

    adding navbar styling

--- jobly-frontend/src/NavBar.js
+++ jobly-frontend/src/NavBar.js
@@ -1,37 +1,38 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import {Navbar} from "reactstrap"
+import "./Home.css"
 
 function NavBar({currentUser}) {
  
     return(
-        <div>
+        <div className="NavBar">
             <Navbar>
-                <NavLink to='/'>
+                <NavLink className="NavBar-NavLink"  to='/'>
                     Jobly
                 </NavLink>
 
-                {currentUser && <NavLink to='/companies'>
+                {currentUser && <NavLink className="NavBar-NavLink"  to='/companies'>
                     Companies
                 </NavLink>}
 
-                {currentUser && <NavLink to='/jobs'>
+                {currentUser && <NavLink className="NavBar-NavLink"  to='/jobs'>
                     Jobs
                 </NavLink>}
 
-                {currentUser && <NavLink to='/profile'>
+                {currentUser && <NavLink className="NavBar-NavLink"  to='/profile'>
                     Profile
                 </NavLink>}
             
-                {!currentUser && <NavLink to='/login'>
+                {!currentUser && <NavLink className="NavBar-NavLink"  to='/login'>
                     Login
                 </NavLink>}
-                {!currentUser &&<NavLink to='/register'>
+                {!currentUser &&<NavLink className="NavBar-NavLink"  to='/register'>
                     SignUp
                 </NavLink>}
                 
                 {currentUser &&
-                <NavLink to='/logout'>
+                <NavLink className="NavBar-NavLink"  to='/logout'>
                     Logout
                 </NavLink>}
             </Navbar>
